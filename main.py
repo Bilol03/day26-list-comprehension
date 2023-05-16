2b4ee6ad19795ea622009d4d63a459d0dae014d9
Bilol Madiyarov <bilol@Bilols-MacBook-Air.local>
2023-05-16 22:10:32 +0900

    Nato Alphabet help mini project using list and dictionary comprehension

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-name = 'Bilol'
-new_list = [letter for letter in name]
-print(new_list)
+# NATO Alphabet help project
+import pandas as pd
+
+file = pd.read_csv('./nato_phonetic_alphabet.csv')
+
+csv_dict = {values.letter: values.code for (keys, values) in file.iterrows()}
+# print(csv_dict)
+
+user_input = input("Enter your name: ")
+
+NATO = [csv_dict[letter.upper()] for letter in user_input]
+print(NATO)
+
+
+
+# number_list = [number for number in range(1,5)]
+# print(number_list)
+#
+# names = ["Bilol", "Shahzod", "Elyor", "Bunyod", "Asad", "Doni"]
+#
+# short_name = [name for name in names if len(name) < 6f]
+# print(short_name)
+
+# file = open()
+
